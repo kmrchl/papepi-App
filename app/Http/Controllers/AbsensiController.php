a8ea8c9ecbf9a1665e7533a426b3c1188b54ec7a
Rachel <93565633+kmrchl@users.noreply.github.com>
2025-01-27 14:59:11 +0700

    Sedikit menambahkan

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\absensi;
 use Illuminate\Http\Request;
 
 class AbsensiController extends Controller
@@ -11,24 +12,66 @@ class AbsensiController extends Controller
      */
     public function index()
     {
-        //
+        $absen = absensi::all();
+
+        return response()->json([
+            'message' => 'Data berhasil ditangkap',
+            'data' => $absen
+        ], 200);
     }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(absensi $id_absen)
+    {
+        $absen = absensi::find($id_absen);
+
+        return response()->json([
+            'message' => 'data berhasil ditangkap',
+            'data' => $absen
+        ]);
+    }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        // Validasi data yang diterima
+        $validated = $request->validate([
+            'id_karyawan' => 'required|string|max:255',
+            'notelp' => 'required|string|max:20',
+            'email' => 'required|email|max:255|unique:karyawan',
+            'alamat' => 'nullable|string|max:255',
+            'posisi' => 'nullable|string|max:255',
+            'jenis_kelamin' => 'required|string',
+            'fingerprint' => 'nullable|string|max:255',
+            'gambar_profil' => 'nullable|string|max:255',
+        ]);
+
+        // Menyimpan data karyawan ke tabel 'karyawan'
+        $karyawan = new absensi();
+        $karyawan->nama = $validated['nama'];
+        $karyawan->notelp = $validated['notelp'];
+        $karyawan->email = $validated['email'];
+        $karyawan->alamat = $validated['alamat'];
+        $karyawan->posisi = $validated['posisi'];
+        $karyawan->jenis_kelamin = $validated['jenis_kelamin'];
+        $karyawan->fingerprint = $validated['fingerprint'];
+        $karyawan->gambar_profil = $validated['gambar_profil'];
+
+        // Simpan data ke database
+        $karyawan->save();
+
+        // Response JSON setelah data berhasil disimpan
+        return response()->json([
+            'message' => 'Data karyawan berhasil ditambahkan',
+            'data' => $karyawan
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
